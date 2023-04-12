0e38eea8b13c2283ea7c70e9a0550cacb158689f
Mark <mshuaic@users.noreply.github.com>
2023-04-12 04:44:34 -0400

    used template to create table

--- script/create_haircut_result_table.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CLICKHOUSE_CLIENT=/usr/bin/clickhouse-client
-TABLE_FOLDER=../table
-
-$CLICKHOUSE_CLIENT \
-    --queries-file $TABLE_FOLDER/haircut_result.sql \
-    --hardware-utilization
