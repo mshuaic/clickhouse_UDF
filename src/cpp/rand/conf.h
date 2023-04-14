35e84663ee08429326d17f2c602ddba327b1c82b
Mark <mshuaic@users.noreply.github.com>
2023-04-14 02:07:50 -0400

    added randomized algo

---
+++ src/cpp/rand/conf.h
@@ -0,0 +1,36 @@
+/* conf.h.  Generated from conf.h.in by configure.  */
+/* conf.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the compiler supports basic C++20 syntax */
+#define HAVE_CXX20 1
+
+/* Define to 1 if you have the `boost_system' library (-lboost_system). */
+#define HAVE_LIBBOOST_SYSTEM 1
+
+/* Define to 1 if you have the `boost_system-mt' library (-lboost_system-mt).
+   */
+/* #undef HAVE_LIBBOOST_SYSTEM_MT */
+
+/* Name of package */
+#define PACKAGE "test"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "test"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "test 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "test"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
---
+++ src/cpp/rand/rand.cpp
@@ -0,0 +1,75 @@
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/algorithm/string.hpp>
+#include <iostream>
+#include <string>
+#include "truncated_normal.hpp"
+#include<ctime>
+
+using namespace boost::multiprecision;
+using namespace boost::algorithm;
+using namespace std;
+
+int main(int argc, char **argv) {
+  string seed = argv[1]; to_lower(seed);
+  uint256_t amount = uint256_t(stoi((string)argv[2]) * pow(10, 18));
+
+  uint64_t blk_num;
+  uint64_t tx_index;
+  string txid;
+  string src;
+  string des;
+  uint256_t value;
+  uint256_t balance;
+
+  cin.tie(nullptr);
+  cin.sync_with_stdio(false);
+
+  cout.tie(nullptr);
+  cout.sync_with_stdio(false);
+
+  unordered_map<string, uint256_t> tainted{{seed, amount}};
+
+  int rseed = time(0);
+
+  while (cin >> blk_num >> tx_index >> txid >> src >> des >> value >> balance) {
+    if (tainted.contains(src) == false
+        || tainted[src] == 0
+        || balance == 0
+        || value == 0) {
+      continue;
+    }
+
+    tainted[src] = min(balance, tainted[src]);
+    uint256_t scale = uint256_t(pow(10, 18));
+    double a = 0;
+    double b = double(tainted[src] / scale);
+    double mean = double(tainted[src] * value / balance / scale);
+    double std = 1;
+    
+    // Define the truncated distribution
+    double x = truncated_normal_ab_sample(mean, std, a, b, rseed);
+    uint256_t taint_amount = uint256_t(x * pow(10, 18));
+
+    // uint256_t taint_amount = 0;
+    if (taint_amount == 0)
+      continue;
+    tainted[des] += taint_amount;
+    tainted[src] -= taint_amount;
+    if (tainted[src] == 0)
+      tainted.erase(src);
+
+    cout << to_string(blk_num) << "\t"
+         << to_string(tx_index) << "\t"
+         << txid << "\t"
+         << src << "\t"
+         << des << "\t"
+         << value.str() << "\t"
+         << balance.str() << "\t"
+         << taint_amount.str() << "\t"
+         << tainted[src].str() << "\t"
+         << tainted[des].str() << endl;
+    cout.flush();
+  }
+
+  return 0;
+}
